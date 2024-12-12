6a666a34de2b4c2249f1b940179d59c5a8807964
federicascognamiglio <scognamigliofede99@gmail.com>
2024-12-12 17:12:10 +0100

    refactoring

--- src/components/AppCard.jsx
+++ src/components/AppCard.jsx
@@ -5,10 +5,10 @@ function AppCard() {
     return (
         <div className={style.card}>
             <div className={style.row}>
-                <div className={style.col-60}>
+                <div className={style.col60}>
                     <img src="/images/placeholder.JPG" alt="" />
                 </div>
-                <div className={style.col-40}>
+                <div className={style.col40}>
                     <div className={style.description}>
                         <h4 className={style.title}>Titolo del Post</h4>
                         <p className={style.caption}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio et voluptates eveniet earum iusto? Accusantium harum cum, ullam explicabo repudiandae consequuntur quaerat. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas omnis quod nostrum id suscipit.</p>
