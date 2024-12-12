67851df3350c0c9e7a2640f8749602697cab771a
federicascognamiglio <scognamigliofede99@gmail.com>
2024-12-12 15:43:08 +0100

    feat: footer completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import AppHeader from "./components/AppHeader";
 import AppMain from "./components/AppMain";
+import AppFooter from "./components/AppFooter";
 
 function App() {
 
   return (
     <>
-      <AppHeader />
-      <AppMain />
+      <div className="wrapper">
+        <AppHeader />
+        <AppMain />
+        <AppFooter />
+      </div>
     </>
   )
 }
